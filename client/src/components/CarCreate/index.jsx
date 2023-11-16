33f81baa28cf0363ca50906c0a7769631c4c6391
OrcaSoftWall <checkanica@gmail.com>
2023-11-16 18:55:38 +0100

    Create form added

--- client/src/components/CarCreate/index.jsx
+++ client/src/components/CarCreate/index.jsx
@@ -6,31 +6,32 @@ function CarCreate() {
             <form id='create'>
                 <div className={styles.container}>
                     <h1>Add a Silk Car</h1>
-                    <label htmlFor='car-make'>Make:</label>
+                    <label htmlFor='make'>Make:</label>
                     <input type='text' id='make' placeholder='Car brand' />
 
-                    <label htmlFor='car-model'>Model:</label>
+                    <label htmlFor='model'>Model:</label>
                     <input type='text' id='model' placeholder='Car model' />
 
-                    <label htmlFor='car-type'>Type:</label>
-                    <select id='make'>
-                        <option value='F1' selected>F1</option>
-                        <option value='Prototype'>Prototype</option>
-                        <option value='Electric'>Electric</option>
-                        <option value='Sedan'>Sedan</option>
+                    <label htmlFor='type'>Type:</label>
+                    <select id='type'>
+                        <option value='F1' defaultValue>F1</option>
                         <option value='Van'>Van</option>
                         <option value='SUV'>SUV</option>
                         <option value='Truck'>Truck</option>
+                        <option value='Sedan'>Sedan</option>
+                        <option value='Electric'>Electric</option>
                         <option value='Hatchback'>Hatchback</option>
+                        <option value='Prototype'>Prototype</option>
                     </select>
 
                     <label htmlFor='picture'>Pcture URL:</label>
                     <input type='text' id='picture' placeholder='Car picture URL' />
 
-                    <label htmlFor='Summary'>Summary:</label>
+                    <label htmlFor='summary'>Summary:</label>
                     <input type='textarea' id='summary' placeholder='Dream car...' />
 
-                    <input className={styles.submitBtn} type='submit' value='Create car' />
+                    <input className={styles.submitBtn} type='submit' value='CREATE' />
+                    <input className={styles.submitBtn} type='reset' value='RESET FORM' />
                 </div>
             </form>
         </section>
