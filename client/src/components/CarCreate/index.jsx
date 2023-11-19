49d6aefc40fe6741925bc2a6f30bb4f1cd7e4adf
OrcaSoftWall <checkanica@gmail.com>
2023-11-19 22:44:37 +0100

    feat: careateCar (tbc)

--- client/src/components/CarCreate/index.jsx
+++ client/src/components/CarCreate/index.jsx
@@ -1,19 +1,27 @@
 import styles from './index.module.css';
 
+const createCarSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const carData = Object.fromEntries(new FormData(e.currentTarget));
+    console.log(carData);
+}
+
+
 function CarCreate() {
     return (
         <section className={styles.carCreate}>
-            <form id='create'>
+            <form id='create' onSubmit={createCarSubmitHandler}>
                 <div className={styles.container}>
                     <h1>Add a Silk Car</h1>
                     <label htmlFor='make'>Make:</label>
-                    <input type='text' id='make' placeholder='Car brand' />
+                    <input type='text' id='make' name='make' placeholder='Car brand' />
 
                     <label htmlFor='model'>Model:</label>
-                    <input type='text' id='model' placeholder='Car model' />
+                    <input type='text' id='model' name='model' placeholder='Car model' />
 
                     <label htmlFor='type'>Type:</label>
-                    <select id='type'>
+                    <select id='type' name='type'>
                         <option value='F1' defaultValue>F1</option>
                         <option value='Van'>Van</option>
                         <option value='SUV'>SUV</option>
@@ -25,10 +33,10 @@ function CarCreate() {
                     </select>
 
                     <label htmlFor='picture'>Pcture URL:</label>
-                    <input type='text' id='picture' placeholder='Car picture URL' />
+                    <input type='text' id='picture' name='imageURL' placeholder='Car picture URL' />
 
                     <label htmlFor='summary'>Summary:</label>
-                    <input type='textarea' id='summary' placeholder='Dream car...' />
+                    <textarea id='summary' name='summary' placeholder='Dream car...' />
 
                     <input className={styles.submitBtn} type='submit' value='CREATE' />
                     <input className={styles.submitBtn} type='reset' value='RESET FORM' />
