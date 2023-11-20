8c4aea1d86fcb4b76a1a50c7140f89d76fa4b946
OrcaSoftWall <checkanica@gmail.com>
2023-11-20 17:42:49 +0100

    Update index.jsx
    to controlled form

--- client/src/components/Login/index.jsx
+++ client/src/components/Login/index.jsx
@@ -1,17 +1,25 @@
 import { Link } from 'react-router-dom';
 import styles from './index.module.css';
+import useForm from '../../hooks/useForm';
+
 
 function Login() {
+const {CSSFontFeatureValuesRule, onChange, onSubmit} = useForm({
+    email: '',
+    password: '',
+});
+
+
     return (
         <section id="login-page" className={styles.login}>
-            <form id='login'>
+            <form id='login' onSubmit={onSubmit}>
                 <div className={styles.container}>
                     <h1>Login</h1>
                     <label htmlFor='email'>Email:</label>
-                    <input type='email' id='email' name='email' placeholder='user@domain.com' />
+                    <input type='email' id='email' name='email' placeholder='user@domain.com' onChange={onChange} value={values['email']} />
 
                     <label htmlFor='login-pass'>Password:</label>
-                    <input type='password' id='login-password' name='password' />
+                    <input type='password' id='login-password' name='password'  onChange={onChange} value={values['password']} />
 
                     <input className={styles.submitBtn} type='submit' value='Login' />
                     <p className={styles.field}>
