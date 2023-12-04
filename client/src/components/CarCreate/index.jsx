aefe3b8994bf7f1302b0c4db9052fbec8cd62b36
OrcaSoftWall <checkanica@gmail.com>
2023-12-04 23:30:49 +0100

    Update index.jsx

--- client/src/components/CarCreate/index.jsx
+++ client/src/components/CarCreate/index.jsx
@@ -1,12 +1,20 @@
 import styles from './index.module.css';
 import { useNavigate } from 'react-router-dom';
 import * as carsService from '../../services/carsService'
+import useForm from '../../hooks/useForm';
 
 
 
 
 function CarCreate() {
     const navigate = useNavigate();
+    const {formValues,onChangeHandler} = useForm({
+        make:'',
+        model: '',
+        type: '',
+        imageURL: '',
+        summary: ''
+    })
 
     const createCarSubmitHandler = async (e) => {
         e.preventDefault();
@@ -15,27 +23,23 @@ function CarCreate() {
         try {
             const result = await carsService.create(carData);
             console.log(result)
-            navigate('/');
+            navigate('/cars/gallery');
         } catch (err) {
             // Error notification
             console.log(err);
         }
     }
 
-    
     // const editCarSubmitHandler = async (e) => {
     //     e.preventDefault()
     //     const values = Object.fromEntries(new FormData(e.currentTarget))
     //     try {
             
-    //         navigate('/gallery')
+    //         navigate('/cars/gallery')
     //     } catch (error) {
     //         console.log(error)
     //     }
     // }
-
-
-
 
     return (
         <section className={styles.carCreate}>
@@ -43,13 +47,13 @@ function CarCreate() {
                 <div className={styles.container}>
                     <h1>Add a Silk Car</h1>
                     <label htmlFor='make'>Make:</label>
-                    <input type='text' id='make' name='make' placeholder='Car brand' />
+                    <input type='text' id='make' name='make' placeholder='Car brand' value={formValues} onChange={onChangeHandler} />
 
                     <label htmlFor='model'>Model:</label>
-                    <input type='text' id='model' name='model' placeholder='Car model' />
+                    <input type='text' id='model' name='model' placeholder='Car model' value={formValues} onChange={onChangeHandler} />
 
                     <label htmlFor='type'>Type:</label>
-                    <select id='type' name='type'>
+                    <select id='type' name='type' value={formValues} onChange={onChangeHandler} >
                         <option value='F1' defaultValue>F1</option>
                         <option value='Van'>Van</option>
                         <option value='SUV'>SUV</option>
@@ -61,10 +65,10 @@ function CarCreate() {
                     </select>
 
                     <label htmlFor='picture'>Pcture URL:</label>
-                    <input type='text' id='picture' name='imageURL' placeholder='Car picture URL' />
+                    <input type='text' id='picture' name='imageURL' placeholder='Car picture URL' value={formValues} onChange={onChangeHandler} />
 
                     <label htmlFor='summary'>Summary:</label>
-                    <textarea id='summary' name='summary' placeholder='Dream car...' />
+                    <textarea id='summary' name='summary' placeholder='Dream car...' value={formValues} onChange={onChangeHandler} />
 
                     <input className={styles.submitBtn} type='submit' value='CREATE' />
                     <input className={styles.submitBtn} type='reset' value='RESET FORM' />
