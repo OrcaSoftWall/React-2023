9b0c06885ac42d6c44e50065ca8283f057698a8c
OrcaSoftWall <117041625+OrcaSoftWall@users.noreply.github.com>
2023-12-06 13:55:37 +0100

    fix: AddCar refactored to controlled form

--- client/src/components/CarCreate/index.jsx
+++ client/src/components/CarCreate/index.jsx
@@ -3,38 +3,54 @@ import { useNavigate } from 'react-router-dom';
 import * as carsService from '../../services/carsService'
 import useForm from '../../hooks/useForm';
 
-
+const formValuesKeys = {
+    Make: 'make',
+    Model: 'model',
+    Type: 'type',
+    Image: 'imageURL',
+    Summary: 'summary',
+}
 
 
 function CarCreate() {
     const navigate = useNavigate();
-    const {formValues,onChangeHandler} = useForm({
-        make:'',
-        model: '',
-        type: '',
-        imageURL: '',
-        summary: ''
-    })
-
-    const createCarSubmitHandler = async (e) => {
-        e.preventDefault();
-
-        const carData = Object.fromEntries(new FormData(e.currentTarget));
+    const { values, onChange, onSubmit } = useForm(async (values) => {
+        console.log("CarCreate Function values: ", values);
         try {
-            const result = await carsService.create(carData);
+            const result = await carsService.create(values);
             console.log(result)
             navigate('/cars/gallery');
         } catch (err) {
             // Error notification
             console.log(err);
         }
-    }
+    },{
+        [formValuesKeys.make]: '',
+        [formValuesKeys.model]: '',
+        [formValuesKeys.type]: '',
+        [formValuesKeys.imageURL]: '',
+        [formValuesKeys.summary]: ''
+    })
+
+    // const createCarSubmitHandler = async (e) => {
+    //     e.preventDefault();
+
+    //     const carData = Object.fromEntries(new FormData(e.currentTarget));
+    //     try {
+    //         const result = await carsService.create(carData);
+    //         console.log(result)
+    //         navigate('/cars/gallery');
+    //     } catch (err) {
+    //         // Error notification
+    //         console.log(err);
+    //     }
+    // }
 
     // const editCarSubmitHandler = async (e) => {
     //     e.preventDefault()
     //     const values = Object.fromEntries(new FormData(e.currentTarget))
     //     try {
-            
+
     //         navigate('/cars/gallery')
     //     } catch (error) {
     //         console.log(error)
@@ -43,17 +59,17 @@ function CarCreate() {
 
     return (
         <section className={styles.carCreate}>
-            <form id='create' onSubmit={createCarSubmitHandler}>
+            <form id='create' onSubmit={onSubmit}>
                 <div className={styles.container}>
                     <h1>Add a Silk Car</h1>
                     <label htmlFor='make'>Make:</label>
-                    <input type='text' id='make' name='make' placeholder='Car brand' value={formValues} onChange={onChangeHandler} />
+                    <input type='text' id='make' name={formValuesKeys.Make} placeholder='Car brand' value={values[formValuesKeys.Make]} onChange={onChange} />
 
                     <label htmlFor='model'>Model:</label>
-                    <input type='text' id='model' name='model' placeholder='Car model' value={formValues} onChange={onChangeHandler} />
+                    <input type='text' id='model' name={formValuesKeys.Model} placeholder='Car model' value={values[formValuesKeys.Model]} onChange={onChange} />
 
                     <label htmlFor='type'>Type:</label>
-                    <select id='type' name='type' value={formValues} onChange={onChangeHandler} >
+                    <select id='type' name={formValuesKeys.Type} value={values[formValuesKeys.Type]} onChange={onChange} >
                         <option value='F1' defaultValue>F1</option>
                         <option value='Van'>Van</option>
                         <option value='SUV'>SUV</option>
@@ -65,10 +81,10 @@ function CarCreate() {
                     </select>
 
                     <label htmlFor='picture'>Pcture URL:</label>
-                    <input type='text' id='picture' name='imageURL' placeholder='Car picture URL' value={formValues} onChange={onChangeHandler} />
+                    <input type='text' id='picture' name={formValuesKeys.Image} placeholder='Car picture URL' value={values[formValuesKeys.Image]} onChange={onChange} />
 
                     <label htmlFor='summary'>Summary:</label>
-                    <textarea id='summary' name='summary' placeholder='Dream car...' value={formValues} onChange={onChangeHandler} />
+                    <textarea id='summary' name={formValuesKeys.Summary} placeholder='Dream car...' value={values[formValuesKeys.Summary]} onChange={onChange} />
 
                     <input className={styles.submitBtn} type='submit' value='CREATE' />
                     <input className={styles.submitBtn} type='reset' value='RESET FORM' />
